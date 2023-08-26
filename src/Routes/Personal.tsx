a2cc779180224b37dbf0144a171a513701cc9047
landlala <land_lala@naver.com>
2023-08-26 23:56:11 +0900

    day9: personal word 1

--- src/Routes/Personal.tsx
+++ src/Routes/Personal.tsx
@@ -1,26 +1,69 @@
 import styled from "styled-components";
 import transition from "../transition";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
+    position: relative;
+    margin-top: 10vh;
     width: 100vw;
-    height: 100vh;
-    display: flex;
+    height: 75vh;
+    overflow: hidden;
 `;
 
 const Bg = styled.img`
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width : 100vw;
-    height: 90vh;
+    top: -10vh;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
 `;
 
+const Word = styled(motion.img)`
+    width: 20%;
+`;
 
 function Personal() {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
-        <Wrapper>
-            <Bg src = "../../Images/bg1.png" />
+        <Wrapper ref = {biggerBoxRef}>
+            <Bg src = "../../Images/edit11.jpg" />
+            <Word 
+                src = "../../Images/words/신록.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+            <Word 
+                src = "../../Images/words/자투리.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+            <Word 
+                src = "../../Images/words/우수.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+            <Word 
+                src = "../../Images/words/선연하게.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+            <Word 
+                src = "../../Images/words/무르녹은.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+            <Word 
+                src = "../../Images/words/단편적.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+            <Word 
+                src = "../../Images/words/형이상학.png" 
+                drag
+                dragConstraints = {biggerBoxRef}
+            />
+
         </Wrapper>
     );
 }
