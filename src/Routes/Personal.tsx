2b55d854d0409320c86bea6ad8a9ac488bac267a
landlala <land_lala@naver.com>
2023-08-28 20:40:55 +0900

    day10: personal word 2

--- src/Routes/Personal.tsx
+++ src/Routes/Personal.tsx
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 import transition from "../transition";
 import { motion } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
     position: relative;
     margin-top: 10vh;
     width: 100vw;
     height: 75vh;
     overflow: hidden;
+`;
+
+const Logo = styled.img`
+    position: absolute;
+    top: 1vh;
+    right: 0;
+    left: 0;
+    margin: auto;
+    height: 7%;
 `;
 
 const Bg = styled.img`
@@ -20,51 +30,147 @@ const Bg = styled.img`
 `;
 
 const Word = styled(motion.img)`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
     width: 20%;
 `;
 
-function Personal() {
-    const biggerBoxRef = useRef<HTMLDivElement>(null);
-    return (
-        <Wrapper ref = {biggerBoxRef}>
-            <Bg src = "../../Images/edit11.jpg" />
-            <Word 
-                src = "../../Images/words/신록.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
-            <Word 
-                src = "../../Images/words/자투리.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
-            <Word 
-                src = "../../Images/words/우수.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
-            <Word 
-                src = "../../Images/words/선연하게.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
-            <Word 
-                src = "../../Images/words/무르녹은.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
-            <Word 
-                src = "../../Images/words/단편적.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
-            <Word 
-                src = "../../Images/words/형이상학.png" 
-                drag
-                dragConstraints = {biggerBoxRef}
-            />
+const Specific = styled(motion.img)`
+    position: absolute;
+    bottom: 3.5vh;
+    right: 0;
+    left: 0;
+    margin: auto;
+    height: 7%;
+`;
 
-        </Wrapper>
+function Personal() {
+    const biggerBoxRef = useRef(null);
+    const [clickedWord, setClickedWord] = useState(-1);
+    const wordHandler = (id: number) => {
+        setClickedWord(id);
+    };
+    return (
+        <>
+            <Link to = "/">
+                <Logo src = "../../Images/logo/1.png" />
+            </Link>
+            <Wrapper ref = {biggerBoxRef}>
+                <Bg src = "../../Images/edit11.jpg" />
+                <Word 
+                    src = "../../Images/words/0.png"
+                    onClick = {() => wordHandler(0)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/1.png"
+                    onClick = {() => wordHandler(1)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/2.png"
+                    onClick = {() => wordHandler(2)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/3.png"
+                    onClick = {() => wordHandler(3)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/4.png"
+                    onClick = {() => wordHandler(4)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/5.png"
+                    onClick = {() => wordHandler(5)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/6.png"
+                    onClick = {() => wordHandler(6)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/7.png"
+                    onClick = {() => wordHandler(7)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/8.png"
+                    onClick = {() => wordHandler(8)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/9.png"
+                    onClick = {() => wordHandler(9)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/10.png"
+                    onClick = {() => wordHandler(10)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/11.png"
+                    onClick = {() => wordHandler(11)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+                <Word 
+                    src = "../../Images/words/12.png"
+                    onClick = {() => wordHandler(12)}
+                    drag
+                    dragConstraints = {biggerBoxRef}
+                    whileTap={{cursor: "grabbing"}}
+                    whileDrag = {{opacity: 0.5}}
+                />
+            </Wrapper>
+            {clickedWord !== -1 ? 
+                <Specific src = {`../../Images/words/specific/${clickedWord}.png`} />
+                : null
+            }
+        </>
     );
 }
 
